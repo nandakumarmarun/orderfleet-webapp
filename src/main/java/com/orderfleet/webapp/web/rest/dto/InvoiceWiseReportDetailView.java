ba0ae5256803a1a76a3f9bdfd1ee2b9831724c4e
Android <Android@Android-PC>
2019-06-10 13:42:38 +0530

    Invoicewise Report

---
+++ src/main/java/com/orderfleet/webapp/web/rest/dto/InvoiceWiseReportDetailView.java
@@ -0,0 +1,83 @@
+package com.orderfleet.webapp.web.rest.dto;
+
+public class InvoiceWiseReportDetailView {
+
+	private String pid;
+
+	private String documentName;
+
+	private double documentTotal;
+	
+	private double documentVolume;
+
+	private String documentType;
+	
+	private boolean imageSaved;
+
+	public InvoiceWiseReportDetailView(String pid, String documentName, double documentTotal,
+			String documentType) {
+		super();
+		this.pid = pid;
+		this.documentName = documentName;
+		this.documentTotal = documentTotal;
+		this.documentType = documentType;
+	}
+
+	public InvoiceWiseReportDetailView(String pid, String documentName, String documentType, boolean imageSaved) {
+		super();
+		this.pid = pid;
+		this.documentName = documentName;
+		this.documentType = documentType;
+		this.imageSaved = imageSaved;
+	}
+
+	public String getPid() {
+		return pid;
+	}
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+
+	public String getDocumentName() {
+		return documentName;
+	}
+
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+
+	public double getDocumentTotal() {
+		return documentTotal;
+	}
+
+	public void setDocumentTotal(double documentTotal) {
+		this.documentTotal = documentTotal;
+	}
+	
+	public double getDocumentVolume() {
+		return documentVolume;
+	}
+
+	public void setDocumentVolume(double documentVolume) {
+		this.documentVolume = documentVolume;
+	}
+
+	public String getDocumentType() {
+		return documentType;
+	}
+
+	public void setDocumentType(String documentType) {
+		this.documentType = documentType;
+	}
+
+	public boolean getImageSaved() {
+		return imageSaved;
+	}
+
+	public void setImageSaved(boolean imageSaved) {
+		this.imageSaved = imageSaved;
+	}
+
+	
+}
