aa8d6f1590cd6a236361a2388db3f25e5cfb6126
A1 <A1@AitrichA1>
2020-12-08 17:01:30 +0530

    Account Profile Geo Tag Api

--- src/main/webapp/resources/app/process-flow-stage1.js
+++ src/main/webapp/resources/app/process-flow-stage1.js
@@ -46,8 +46,23 @@ if (!this.InventoryVoucher) {
 		$("#btnApply").on('click', function() {
 			InventoryVoucher.filter();
 		});
+		
+		$('#btnDownload').on('click', function() {
+			var tblProcessFlow = $("#tblProcessFlow tbody");
+			if (tblProcessFlow.children().length == 0) {
+				alert("no values available");
+				return;
+			}
+			var name = "inflow"
+			downloadXls1(name);
+		});
 
 	});
+	
+	function downloadXls1(excelName) {
+		 var table2excel = new Table2Excel();
+	     table2excel.export(document.getElementById('tblProcessFlow'),excelName);
+	}
 
 	function loadAllDocumentByDocumentType() {
 		if ($('#dbDocumentType').val() == "no") {
