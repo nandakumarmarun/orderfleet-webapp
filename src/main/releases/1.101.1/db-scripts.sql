6dc2fd03866e76909e93b315fb14c5483b90f48d
Rameeskr77 <rameeskr77@gmail.com>
2022-10-05 20:43:20 +0530

    "deleted"

--- src/main/releases/1.101.1/db-scripts.sql
+++
@@ -1,8 +0,0 @@
-create table cicd_sample ( 
-   ID INT PRIMARY KEY     NOT NULL,
-   NAME           TEXT    NOT NULL,
-   Description        CHAR(50));
-   
-   
-   
-INSERT INTO cicd_sample (ID, NAME, Description) VALUES (2, 'jithin', 'javaDeveloper');
