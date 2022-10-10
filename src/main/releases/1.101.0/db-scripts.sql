9300305c94900d584b110e8faa5f70dcace7e53c
Rameeskr77 <rameeskr77@gmail.com>
2022-10-10 13:30:03 +0530

    "idempotent in query in sql Script"

--- src/main/releases/1.101.0/db-scripts.sql
+++ src/main/releases/1.101.0/db-scripts.sql
@@ -1,8 +1,8 @@
-﻿create table cicd_sample (
+﻿create table IF NOT EXISTS cicd_sample (
    ID INT PRIMARY KEY     NOT NULL,
    NAME           TEXT    NOT NULL,
    Description        CHAR(50));
 
 
 
-INSERT INTO cicd_sample (ID, NAME, Description) VALUES (2, 'ramees', 'javaDeveloper');
+INSERT INTO cicd_sample (ID, NAME, Description) VALUES (3, 'ramees', 'javaDeveloper')ON CONFLICT DO NOTHING;
