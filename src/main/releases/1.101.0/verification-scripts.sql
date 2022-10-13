106442c07f1736aa64d422f9f24f08f58ecfb7ca
Jithin Sethumadhavan <jithins8129@gmail.com>
2022-10-13 12:56:57 +0530

    ver scriot

--- src/main/releases/1.101.0/verification-scripts.sql
+++ src/main/releases/1.101.0/verification-scripts.sql
@@ -1,1 +1,14 @@
-﻿select 'ok'
+﻿DO $$
+BEGIN 
+  IF EXISTS 
+(
+	SELECT 1
+	FROM information_schema.tables 
+	WHERE table_schema = 'public'
+	AND table_name = 'cicd_sample'
+) THEN
+    RAISE NOTICE 'ok';
+  END IF;
+END $$;
+
+
