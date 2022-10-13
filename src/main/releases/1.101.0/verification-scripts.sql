8d7db753f2c03bec5cb758b81220ec7410a452d1
Jithin Sethumadhavan <jithins8129@gmail.com>
2022-10-13 13:48:33 +0530

    verification

--- src/main/releases/1.101.0/verification-scripts.sql
+++ src/main/releases/1.101.0/verification-scripts.sql
@@ -1,13 +1,22 @@
-﻿DO $$
-BEGIN 
-  IF EXISTS 
-(
-	SELECT 1
-	FROM information_schema.tables 
-	WHERE table_schema = 'public'
-	AND table_name = 'cicd_sample'
-) THEN
+﻿create or replace function if_table_exist(sch_name text, tbl_name text)
+returns boolean
+language plpgsql
+as
+$$
+begin
+   IF EXISTS 
+	(
+		SELECT 1
+		FROM information_schema.tables 
+		WHERE table_schema = sch_name
+		AND table_name = tbl_name
+	) THEN
+		RETURN true;
+	ELSE
+		RETURN false;
+	  END IF;
+	  
+end;
+$$;
 
-    RAISE NOTICE 'ok';
-  END IF;
-END $$;
+select if_table_exist('public', 'cicd_sample');
