16aa5b4f910bdefc3286f1e9b8392db0652a0046
Aitrich <prashob.aitrich@gmail.com>
2022-11-05 12:01:44 +0530

    excel upload in gsif

--- src/main/java/com/orderfleet/webapp/web/vendor/excel/service/AccountProfileGsifUploadService.java
+++ src/main/java/com/orderfleet/webapp/web/vendor/excel/service/AccountProfileGsifUploadService.java
@@ -74,7 +74,7 @@ public class AccountProfileGsifUploadService {
 	private final Logger log = LoggerFactory.getLogger(AccountProfileGsifUploadService.class);
 
 	private final BulkOperationRepositoryCustom bulkOperationRepositoryCustom;
-	
+
 	private final SyncOperationRepository syncOperationRepository;
 
 	private final PriceLevelRepository priceLevelRepository;
@@ -91,16 +91,13 @@ public class AccountProfileGsifUploadService {
 
 	private final LocationAccountProfileRepository locationAccountProfileRepository;
 
-	
-
 	private final LocationAccountProfileService locationAccountProfileService;
 
 	private final UserRepository userRepository;
 
-	
 	private final LocationService locationService;
-	
-	boolean flagc =false;
+
+	boolean flagc = false;
 
 	public AccountProfileGsifUploadService(BulkOperationRepositoryCustom bulkOperationRepositoryCustom,
 			SyncOperationRepository syncOperationRepository, PriceLevelRepository priceLevelRepository,
@@ -108,9 +105,9 @@ public class AccountProfileGsifUploadService {
 			AccountProfileService accountProfileService, LocationRepository locationRepository,
 			LocationHierarchyRepository locationHierarchyRepository,
 			LocationAccountProfileRepository locationAccountProfileRepository,
-			
+
 			LocationAccountProfileService locationAccountProfileService, UserRepository userRepository,
-			
+
 			LocationService locationService) {
 		super();
 		this.bulkOperationRepositoryCustom = bulkOperationRepositoryCustom;
@@ -121,11 +118,11 @@ public class AccountProfileGsifUploadService {
 		this.locationRepository = locationRepository;
 		this.locationHierarchyRepository = locationHierarchyRepository;
 		this.locationAccountProfileRepository = locationAccountProfileRepository;
-		
+
 		this.accountProfileService = accountProfileService;
 		this.locationAccountProfileService = locationAccountProfileService;
 		this.userRepository = userRepository;
-		
+
 		this.locationService = locationService;
 	}
 
@@ -138,72 +135,51 @@ public class AccountProfileGsifUploadService {
 		final Long companyId = company.getId();
 		Set<AccountProfile> saveUpdateAccountProfiles = new HashSet<>();
 		// All product must have a division/category, if not, set a default one
-		
+
 		AccountType defaultAccountType = accountTypeRepository.findFirstByCompanyIdOrderByIdAsc(company.getId());
-		
+
 		// find all exist account profiles
 		List<String> apAlias = accountProfileDTOs.stream().map(apDto -> apDto.getAlias().toUpperCase())
 				.collect(Collectors.toList());
 
-		
-		List<AccountProfile> accountProfiles = accountProfileRepository.findAllByCompanyId();
-		
+		List<AccountProfile> accountProfiles = accountProfileRepository.findAllByCompanyId(company.getId());
+		System.out.println("Size :" + accountProfiles.size());
+
 		// all pricelevels
 		List<PriceLevel> tempPriceLevel = priceLevelRepository.findByCompanyId(companyId);
 
-		Set<Long> dectivated = new HashSet<>();
 		
-		if(!accountProfiles.isEmpty() && accountProfiles.size() > 1 && !accountProfileDTOs.isEmpty()) {
-			accountProfiles.remove(0);
-			for(AccountProfile ac :accountProfiles) {
-				flagc = false;
-				if(ac.getCustomerCode()!=null)
-				{
-			accountProfileDTOs.forEach(data -> {
-				
-				if(ac.getCustomerCode().equals(data.getCustomerCode())) {
-					flagc = true;
-				}
-			});
-				}
-			if(!flagc) {
-				dectivated.add(ac.getId());
-			}
-		}}
-			if(!dectivated.isEmpty()) {
-				accountProfileRepository.deactivateAcoundProfileUsingInId(dectivated);
-			}
-		
+
 		for (AccountProfileDTO apDto : accountProfileDTOs) {
 			AccountProfile accountProfile;
-				
-				Optional<AccountProfile> optionalAP = accountProfiles.stream()
-						.filter(pc -> pc.getCustomerId() != null && !pc.getCustomerId().equals("")
-								? pc.getCustomerId().equalsIgnoreCase(String.valueOf(apDto.getCustomerId()))
-								: false)
-						.findAny();
-				if (optionalAP.isPresent()) {
-					accountProfile = optionalAP.get();
-					// if not update, skip this iteration. Not implemented now
-					// if (!accountProfile.getThirdpartyUpdate()) { continue; }
-				} else {
-					accountProfile = new AccountProfile();
-					accountProfile.setPid(AccountProfileService.PID_PREFIX + RandomUtil.generatePid());
-					accountProfile.setCustomerId(apDto.getAlias());
-					accountProfile.setUser(user);
-					accountProfile.setCompany(company);
-					accountProfile.setAccountStatus(AccountStatus.Unverified);
-					accountProfile.setDataSourceType(DataSourceType.TALLY);
-					accountProfile.setImportStatus(true);
-				}
-				accountProfile.setAlias(apDto.getAlias());
+			Optional<AccountProfile> optionalAP = accountProfiles.stream()
+					.filter(pc -> pc.getCustomerId() != null && !pc.getCustomerId().equals("")
+							? pc.getCustomerId().equalsIgnoreCase(apDto.getCustomerId())
+							: false)
+					.findAny();
 
-				accountProfile.setName(apDto.getName());
+			if (optionalAP.isPresent()) {
 
-		
+				accountProfile = optionalAP.get();
+				// if not update, skip this iteration. Not implemented now
+				// if (!accountProfile.getThirdpartyUpdate()) { continue; }
+			} else {
+				accountProfile = new AccountProfile();
+				accountProfile.setPid(AccountProfileService.PID_PREFIX + RandomUtil.generatePid());
+				accountProfile.setCustomerId(apDto.getCustomerId());
+				accountProfile.setUser(user);
+				accountProfile.setCompany(company);
+				accountProfile.setAccountStatus(AccountStatus.Unverified);
+				accountProfile.setDataSourceType(DataSourceType.TALLY);
+				accountProfile.setImportStatus(true);
+			}
+			accountProfile.setAlias(apDto.getAlias());
+
+			accountProfile.setName(apDto.getName());
+
 			accountProfile.setCustomerId(apDto.getCustomerId());
 			accountProfile.setTrimChar(apDto.getTrimChar());
-			accountProfile.setCustomerId(apDto.getAlias());
+
 			if (isValidPhone(apDto.getPhone1())) {
 				accountProfile.setPhone1(apDto.getPhone1());
 			}
@@ -213,9 +189,10 @@ public class AccountProfileGsifUploadService {
 			if (isValidEmail(apDto.getEmail1())) {
 				accountProfile.setEmail1(apDto.getEmail1());
 			}
-			if(apDto.getCustomerCode()!=null) {
-			accountProfile.setCustomerCode(apDto.getCustomerCode());}
-			
+			if (apDto.getCustomerCode() != null) {
+				accountProfile.setCustomerCode(apDto.getCustomerCode());
+			}
+
 			accountProfile.setTinNo(apDto.getTinNo());
 			accountProfile.setPin(apDto.getPin());
 			accountProfile.setDescription(apDto.getDescription());
@@ -246,31 +223,32 @@ public class AccountProfileGsifUploadService {
 					}
 				}
 			}
-			
+
 			if (accountProfile.getAccountType() == null) {
 				accountProfile.setAccountType(defaultAccountType);
 			}
 			List<AccountType> accountTypes = accountTypeRepository.findAllByCompanyId();
-			
-			if (apDto.getAccountTypeName() != null ) {
-			 Optional<AccountType> optionalAccountType = accountTypes.stream()
-			 .filter(atn -> apDto.getAccountTypeName().equals(atn.getName())).findAny();
-			
-			 if (optionalAccountType.isPresent()) {
-			 accountProfile.setAccountType(optionalAccountType.get());
-			 } else {
-			 accountProfile.setAccountType(defaultAccountType);
-			 }
+
+			if (apDto.getAccountTypeName() != null) {
+				Optional<AccountType> optionalAccountType = accountTypes.stream()
+						.filter(atn -> apDto.getAccountTypeName().equals(atn.getName())).findAny();
+
+				if (optionalAccountType.isPresent()) {
+					accountProfile.setAccountType(optionalAccountType.get());
+				} else {
+					accountProfile.setAccountType(defaultAccountType);
+				}
 			}
 			Optional<AccountProfile> opAcp = saveUpdateAccountProfiles.stream()
 					.filter(acp -> acp.getName().equalsIgnoreCase(apDto.getName())).findAny();
-			
+
 			if (opAcp.isPresent()) {
+				System.out.println("optional present");
 				continue;
 			}
 			saveUpdateAccountProfiles.add(accountProfile);
 		}
-		System.out.println("Size :" +saveUpdateAccountProfiles.size());
+		System.out.println("Size :" + saveUpdateAccountProfiles.size());
 		bulkOperationRepositoryCustom.bulkSaveAccountProfile(saveUpdateAccountProfiles);
 		long end = System.nanoTime();
 		double elapsedTime = (end - start) / 1000000.0;
@@ -280,6 +258,27 @@ public class AccountProfileGsifUploadService {
 		syncOperation.setLastSyncTime(elapsedTime);
 		syncOperationRepository.save(syncOperation);
 		log.info("Sync completed in {} ms", elapsedTime);
+		Set<Long> dectivated = new HashSet<>();
+		if (!accountProfiles.isEmpty() && accountProfiles.size() > 1 && !accountProfileDTOs.isEmpty()) {
+			accountProfiles.remove(0);
+			for (AccountProfile ac : accountProfiles) {
+				flagc = false;
+				if (ac.getCustomerCode()!= null) {
+					System.out.println("name :"+ac.getName()+"code :"+ ac.getCustomerCode());
+					accountProfileDTOs.forEach(data -> {
+             	if (ac.getCustomerCode().equals(data.getCustomerCode())) {
+							flagc = true;
+						}
+					});
+					if (!flagc) {
+						dectivated.add(ac.getId());
+					}
+				}
+			}
+		}
+		if (!dectivated.isEmpty()) {
+			accountProfileRepository.deactivateAcoundProfileUsingInId(dectivated);
+		}
 	}
 
 	@Transactional
